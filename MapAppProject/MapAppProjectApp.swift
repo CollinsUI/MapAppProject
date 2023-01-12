4d70c6f9897955221d35bad348819638fe665ff3
Collins Swift <91741002+CollinsUI@users.noreply.github.com>
2023-01-12 09:23:38 +0300

    Initial Commit

---
+++ MapAppProject/MapAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppProjectApp.swift
+//  MapAppProject
+//
+//  Created by Collins Muthomi on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
